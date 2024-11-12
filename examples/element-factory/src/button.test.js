b837b7c4232c3cc59d09d5e77a9a9e711f701616
Lucas Hill <lhill@compunet.biz>
2024-11-11 20:30:11 -0700

    Added some basic tests to the element-factory section of the course

--- examples/element-factory/src/button.test.js
+++ examples/element-factory/src/button.test.js
@@ -1,9 +1,28 @@
+import { screen, fireEvent } from '@testing-library/dom'; //This gives access to the helper functions in the library
+import userEvent from '@testing-library/user-event'; //This helps us simulate user events in our mock DOM
+// import { screen } from '@testing-library/vue';
+// import { screen } from '@testing-library/react';
 import { createButton } from './button.js';
 
-describe.todo('createButton', () => {
-  it('should create a button element', () => {});
+describe('createButton', () => {
+  //This will clear out the DOM between the tests
+  afterEach(() => {
+    document.body.innerHTML = '';
+  })
 
-  it('should have the text "Click Me"', () => {});
+  it('should create a button element', () => {
+    document.body.appendChild(createButton());
+    const button = screen.getByRole('button', { name: 'Click Me' });
 
-  it('should change the text to "Clicked!" when clicked', async () => {});
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should change the text to "Clicked!" when clicked', async () => {
+    document.body.appendChild(createButton());
+
+    const button = screen.getByRole('button', { name: 'Click Me' });
+    await userEvent.click(button); //simulating a user click
+
+    expect(button.textContent).toBe('Clicked!');
+  });
 });
